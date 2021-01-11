1fd58e684f6eaade267d7df0080faf1e7e0dbadd
masterlink950 <masterlink950@gmail.com>
2021-01-11 16:10:45 -0500

    Add compression and JSON parsing to reqeusts

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import 'module-alias/register';
 import 'tsconfig-paths/register';
 import express from 'express';
 import logger from 'morgan';
-import helmet from 'helmet';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import compression from 'compression';
 import v1 from 'v1';
 
 dotenv.config();
@@ -11,8 +12,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.json());
 app.use(logger('dev'));
 app.use(helmet());
+app.use(compression());
 
 app.use('/v1', v1);
 
