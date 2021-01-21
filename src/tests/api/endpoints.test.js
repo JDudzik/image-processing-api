2d263d3a32a021712112b5c86f8ea76d5dd9ff14
masterlink950 <masterlink950@gmail.com>
2021-01-21 14:21:49 -0500

    Create test for images endpoint

--- src/tests/api/endpoints.test.js
+++ src/tests/api/endpoints.test.js
@@ -3,10 +3,31 @@ import supertest from 'supertest';
 
 const request = supertest(server);
 
-it('checks the root endpoint', async done => {
-  const response = await request
-    .get('/')
-    .expect(200);
-    
+it('checks the root endpoint', async (done) => {
+  const response = await request.get('/')
+
+  expect(response.status).toBe(200);
+
   done();
+})
+
+
+describe('modifyImage endpoint', () => {
+  it('checks existing image succeeds', async (done) => {
+    const response = await request.get('/v1/images/fjord.jpg?width=400');
+
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toBe('image/jpeg');
+
+    done();
+  });
+
+  it('checks missing image fails', async (done) => {
+    const response = await request.get('/v1/images/noimage.jpg?height=100');
+
+    expect(response.status).toBe(400);
+    expect(response.body.status).toBe('FILE_DOES_NOT_EXIST');
+
+    done();
+  });
 })
